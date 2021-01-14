204e4c935ea3381b009c38af14005c951d936687
Ramón <schlosser@itplusx.de>
2021-01-14 17:55:46 +0100

    feat!: dump Gridelements TypoScript dependency (#6)
    Make the headless_gridelement TypoScript standalone without the need of the original Gridelements TypoScript.
    
    BREAKING CHANGE: It's now unnecessary to include the Gridelements TypoScript. Furthermore, including it might break the JSON output.
    
    Relates #5

--- Classes/DataProcessing/GridChildrenProcessor.php
+++ Classes/DataProcessing/GridChildrenProcessor.php
@@ -2,6 +2,9 @@
 
 namespace ITplusX\HeadlessGridelements\DataProcessing;
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+
 /**
  * Fetch records from the database, using the default .select syntax from TypoScript.
  */
@@ -12,6 +15,14 @@ class GridChildrenProcessor extends \GridElementsTeam\Gridelements\DataProcessin
      */
     protected function sortRecordsIntoMatrix()
     {
+        $processedColumns = [];
+        foreach ($this->processedRecordVariables as $key => $processedRecord) {
+            if (!isset($processedColumns[$processedRecord['data']['tx_gridelements_columns']])) {
+                $processedColumns[$processedRecord['data']['tx_gridelements_columns']] = [];
+            }
+            $processedColumns[$processedRecord['data']['tx_gridelements_columns']][] = $processedRecord;
+        }
+
         $processedRows = [];
         if (!empty($this->processedData['data']['tx_gridelements_backend_layout_resolved'])) {
             foreach ($this->processedData['data']['tx_gridelements_backend_layout_resolved']['config']['rows.'] as $rowNumber => $row) {
@@ -19,21 +30,49 @@ class GridChildrenProcessor extends \GridElementsTeam\Gridelements\DataProcessin
                 foreach ($row['columns.'] as $column) {
                     $columns[] = [
                         'config' => $column,
-                        'elements' => array_map(
-                            'trim',
-                            (
-                                array_slice(
-                                    explode('###BREAK###', $this->processedData['data']['tx_gridelements_view_columns'][$column['colPos']]),
-                                    0,
-                                    -1
-                                )
-                            )
-                        )
+                        'elements' => is_array($processedColumns[$column['colPos']]) === true ? $this->processRecords($processedColumns[$column['colPos']]) : []
                     ];
                 }
+
                 $processedRows[]['columns'] = $columns;
             }
         }
+
         return $processedRows;
+    }
+
+    /**
+     * Process the records to be rendered as JSON
+     *
+     * @param array $records The array of records to process
+     * @return array The processed records as JSON
+     */
+    private function processRecords(array $records): array {
+        $processedRecords = [];
+
+        foreach ($records as $record) {
+            $processedRecords[] = $this->renderRecord($record['data']);
+        }
+
+        return $processedRecords;
+    }
+
+    /**
+     * Render a record as JSON by given data array
+     *
+     * @param array $data The record data
+     * @return string The JSON string of the rendered record
+     */
+    private function renderRecord(array $data): string {
+        $recordContentObjectRenderer = GeneralUtility::makeInstance(ContentObjectRenderer::class);
+        $recordContentObjectRenderer->start($data, 'tt_content');
+
+        $conf = [
+            'tables' => 'tt_content',
+            'source' => $data['uid'],
+            'dontCheckPid' => 1
+        ];
+
+        return $recordContentObjectRenderer->cObjGetSingle('RECORDS', $conf);
     }
 }
--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -4,5 +4,5 @@ defined('TYPO3_MODE') || die('Access denied.');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'headless_gridelements',
     'Configuration/TypoScript',
-    'Grid Elements json output for EXT:headless'
+    'Headless Gridelements'
 );
