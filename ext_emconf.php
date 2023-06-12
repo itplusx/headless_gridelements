04f7d2fa9f736e3871928704af755b96c7ad1b54
Ramón <schlosser@itplusx.de>
2023-06-12 17:34:25 +0200

    build(deps): bump gridelements version to v11.0 (#19)
    * build(deps): update version constraints (#18)
    
    * revert: update extension version
    
    * revert: change php version constraint
    
    ---------
    
    Co-authored-by: Philipp Kitzberger <coding@kitze.net>

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
             'php' => '7.2.0-0.0.0',
             'typo3' => '11.5.0-11.5.99',
             'headless' => '3.0.0-3.99.99',
-            'gridelements' => '11.0.0-dev',
+            'gridelements' => '11.0.0-11.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
